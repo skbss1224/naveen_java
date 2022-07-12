657734c77f8f8e0c75a4d6ceb9411f8b29da21bd
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-07-12 09:47:57 +0530

    operator
    java

---
+++ operator.java
@@ -0,0 +1,23 @@
+package neveen;
+
+public class operator {
+
+	public static void main(String[] args) {
+		//data type
+		int naveen=12000;
+		int sasi=4000;
+		int total;
+		total=naveen+sasi;
+		System.out.println("plus operator:"+total);
+		total=naveen-sasi;
+		System.out.println("minus operator:"+total);
+		total=naveen*sasi;
+		System.out.println("mul operator"+total);
+		total=naveen/sasi;
+		System.out.println("div operator"+total);
+		total=naveen%sasi;
+		System.out.println("per operator"+total);
+		
+	}
+
+}
