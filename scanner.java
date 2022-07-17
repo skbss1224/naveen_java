4f5ede0a19db3804284b2b7c178cb12b8c9d17d1
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-07-17 13:56:13 +0530

    scanner
    scanner

---
+++ scanner.java
@@ -0,0 +1,16 @@
+package neveen;
+
+import java.util.Scanner;
+
+public class scanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner naveen=new Scanner(System.in);
+		int a=naveen.nextInt();
+		int b=naveen.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+	}
+
+}
