38077ba3080853330f2717793e4284c9c5a03404
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-07-21 09:51:10 +0530

    unery
    unery

--- unery_operator.java
+++ unery_operator.java
@@ -10,15 +10,31 @@ public class unery_operator {
 		//++ increment operator
 		//-- degrement operator
 		int a=5;
-//		System.out.println(a++);//post increment
-//		System.out.println(a);
-//		System.out.println(a++);
-//		System.out.println(a);
+		System.out.println(a++);//post increment
+		System.out.println(a);
+		System.out.println(a++);
+		System.out.println(a);
 		
 		System.out.println(++a);//pre increment
 		System.out.println(a);
 		System.out.println(++a);
 		System.out.println(a);
+		System.out.println(a--);//post degrement
+		System.out.println(a);
+		System.out.println(a--);
+		System.out.println(a);
+		System.out.println(--a);//pre degrement
+		System.out.println(a);
+		System.out.println(--a);
+		System.out.println(a);
+		
+		int a=5,b=6;
+		System.out.println(a++ + ++a + a++ + ++a);
+						//(5+7+7+9)
+						//(12+16)
+		System.out.println(++b - ++a + a++ + b++);
+						//(7-6+6+7)
+						//(1+13)
 	}
 
 }
