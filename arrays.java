5fe21f651104ca0bdcbf91a289e061f8e79baac4
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-09-13 20:09:22 +0530

    array,swapping,secondlargest,sorting
    java

---
+++ arrays.java
@@ -0,0 +1,90 @@
+package neveen;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//	aarays
+//		int[]student_id= {3,4,5,6,7,8,44,55,66,77};
+//		System.out.println(student_id[0]);
+//		System.out.println(student_id[4]);
+//		System.out.println(student_id[7]);
+		
+		
+//		int[]number=new int[4];
+//		number[0]=10;
+//		number[1]=20;
+//		number[2]=30;
+//		System.out.println(number[1]);
+//		System.out.println(number[2]);
+		
+//		String[]student_id1= {"sivamani","billa","akash","vedha"};
+//		System.out.println(student_id1[0]);
+//		System.out.println(student_id1[1]);
+		
+//		List<String>sasi=new ArrayList<String>();
+//		sasi.add("");
+//		System.out.println("empty array list"+sasi);
+//		sasi.add("mango");
+//		System.out.println("add one one arraylist"+sasi);
+//		sasi.add("cherry");
+//		System.out.println("add two arraylist"+sasi);
+//		sasi.add("srawberry");
+//		System.out.println("add three arraylist"+sasi);
+//		sasi.remove("mango");
+//		System.out.println("remove  one arraylist"+sasi);
+//		sasi.remove("");
+//		System.out.println("remove empty  arraylist"+sasi);
+//		sasi.clear();
+//		System.out.println("delete all values"+sasi);
+		
+		//swapping
+//		float first=12.0f,second=24.5f;
+//		System.out.println("--before swapping--");
+//		System.out.println("first number="+first);
+//		System.out.println("second number="+second);
+//		
+//		first=first-second;
+//		second=first+second;
+//		first=second-first;
+//		
+//		System.out.println("--after swap--");
+//		System.out.println("first number="+first);
+//		System.out.println("second number="+second);
+		
+		//swapping 2
+		
+//		float first=1.20f,second=2.45f;
+//		System.out.println("--before swapping--");
+//		System.out.println("first number"+first);
+//		System.out.println("second number"+second);
+//		
+//		float temporary=first;
+//		first=second;
+//		second=temporary;
+//		System.out.println("--after swapping--");
+//		System.out.println("first number="+first);
+//		System.out.println("second number="+second);
+		
+		
+		//second largest number
+//		int array[]= {10,20,25,63,96,57};
+//		int size=array.length;
+//		System.out.println(size);
+//	    Arrays.sort(array);
+//	    System.out.println("sorted array"+Arrays.toString(array));
+//	    int res=array[size-1];
+//	    System.out.println("second largest element is::"+res);
+		
+		//sorting
+		int[]array= {4,45,3,4,8,7,4,22,4,5,6,3,2,5};
+		Arrays.sort(array);
+		System.out.println(Arrays.toString(array));
+	    
+	}
+
+}
